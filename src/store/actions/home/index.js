e8ca5591cda858d6225fd427b877aa2e992e5e2d
tukuna30 <tukuna.patro@gmail.com>
2024-02-23 09:32:11 +0530

    fix search

--- src/store/actions/home/index.js
+++ src/store/actions/home/index.js
@@ -11,6 +11,13 @@ const setUsers = (data) => {
 export const fetchUsers = (query) => async (dispatch) => {
   try {
     const data = await api.getUsers(query);
+
+    console.log(data, 'users', query);
+
+    if (query.keyword) {
+      data.results = data.results.filter(item => item.name.first.includes(query.keyword) || item.login.username.includes(query.keyword) || item.email.includes(query.keyword) )
+    }
+
     dispatch(setUsers(data));
   } catch (err) {
     console.error(err);
